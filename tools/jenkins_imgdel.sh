8db3229233b0fbea4c811d5a8c3cda297eee9e43
Joey Jiang <ziyij@qti.qualcomm.com>
2019-05-10 10:52:14 +0800

    fix some typo

--- tools/jenkins_imgdel.sh
+++ tools/jenkins_imgdel.sh
@@ -5,7 +5,7 @@ for folder in $FOLDER1 $FOLDER2 $FOLDER3
 do
 	FOLDER="L"`echo $folder | awk -FL '{print $2}' | sed 's/;//g'`
 	echo $FOLDER
-	if [ -d "/image/vol00/${FOLDER}..${BUILD_USER}.img" ]; then
+	if [ -f "/images/vol00/${FOLDER}..${BUILD_USER}.img" ]; then
 		killlist="$FOLDER..$BUILD_USER.img"
 		echo $killlist
 		#imgdel.sh -i $killlist 
